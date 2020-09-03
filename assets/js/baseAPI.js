3058de235522443d56aa69f42e67b0b861111f3b
tiancaizs <506066270@qq.com>
2020-09-03 19:13:37 +0800

    完成了主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -5,5 +5,24 @@ $.ajaxPrefilter(function(options) {
     // console.log(options.url);
     // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
     options.url = 'http://ajax.frontend.itheima.net' + options.url
-    console.log(options.url);
+        // console.log(options.url);
+
+    // 统一为有权限的接口设置请求头
+    if (options.url.indexOf('/my/') != -1) {
+        options.headers = { Authorization: localStorage.getItem('token') || '' }
+    }
+
+
+    // 全局挂载 complete 回调函数
+    options.complete = function(res) {
+        console.log('执行了 complete 回调函数');
+        console.log(res);
+        // 在 complete 回调函数中， 可以使用res.responseJSON拿到服务器响应回来的数据
+        if (res.responseJSON.status === 1) {
+            // 1.强制清空 token
+            localStorage.removeItem('token')
+                // 2.强制跳转到登录页面
+            location.href = '/login.html'
+        }
+    }
 })
